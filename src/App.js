45f44e0377ca3cf6df08b879c15a7b014c3fbff0
Suryansh Pandey <suryanshpsurya@gmail.com>
2021-06-15 16:54:48 +0530

    update game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react"
-import Board from "./components/Board"
-import './components/root.scss'
+import React from "react";
+import Board from "./components/Board";
+import './components/root.scss';
+
 export default () => (
   <>
     <h1>Tic Tac Toe Game</h1>
